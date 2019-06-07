8ec120336b7f48d43b80bd49bd7594e6640d0eb5
Julian Edwards <julian.edwards@mongodb.com>
2019-06-07 10:48:54 -0400

    fix up installs script

--- installs.sh
+++ installs.sh
@@ -1,7 +1,11 @@
+SEPARATOR="\n=====================================\n"
 CUR_USER=$1
 if [[ "$CUR_USER" == "" ]]; then
     CUR_USER=julianedwards
 fi
+echo "User is $CUR_USER"
+
+echo $SEPARATOR
 
 # install homebrew
 if [[ $(command -v brew) == "" ]]; then 
@@ -12,6 +16,8 @@ else
     sudo -u $CUR_USER brew update
 fi
 
+echo $SEPARATOR
+
 # install vim-plug
 #       install plugins via vim -- :PlugInstall
 #       install vim-go binaries: :GoInstallBinaries
@@ -19,18 +25,23 @@ VIMPLUG=~/.vim/autoload/plug.vim
 if [[ -f "$VIMPLUG" ]]; then
     echo "vim-plug installed"
 else
-    echo "Installing vim-plug"
+    echo "Installing vim-plug..."
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
+echo $SEPARATOR
+
 # install mongo
+MONGO_VERSION=4.0
 if [[ $(command -v mongo) == "" ]]; then
+    echo "Installing MongoDB version $MONGO_VERSION..."
     sudo -u $CUR_USER brew tap mongodb/brew
-    sudo -u $CUR_USER brew install mongodb-community@4.0
+    sudo -u $CUR_USER brew install mongodb-community@$MONGO_VERSION
     mkdir /data
     mkdir /data/db
     chmod 0755 /data/db && chown $CUR_USER /data/db
 else
     echo "MongoDB installed"
+    mongod --version
 fi
